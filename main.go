46da55fb0eb08af73bbc1345202b1de2e5f29b61
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2025-05-12 13:15:29 -0700

    simple command parser and support for PING and ECHO command

--- main.go
+++ main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", "localhost:6379")
 	if err != nil {
 		log.Fatal("Failed to bind to port 6379")
 	}
 	log.Println("Listening on port 6379")
 
-	_, err = l.Accept()
-	if err != nil {
-		log.Fatal("Error accepting connection: ", err.Error())
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("Error accepting connection: ", err.Error())
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		cmd, err := parseCommand(conn)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		switch cmd.name {
+		case "ping":
+			conn.Write([]byte("+PONG\r\n"))
+		case "echo":
+			for _, arg := range cmd.args {
+				resp := fmt.Sprintf("$%d\r\n%s\r\n", len(arg), arg)
+				conn.Write([]byte(resp))
+			}
+		}
 	}
 }
---
+++ parse.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type commandState = int
+
+const (
+	commandStateParsingCmd = iota
+	commandStateParsingArgs
+	commandStateDone
+)
+
+type Command struct {
+	name  string
+	args  []string
+	state commandState
+}
+
+func parseCommand(conn io.Reader) (*Command, error) {
+	client := bufio.NewScanner(conn)
+
+	cmd := &Command{
+		state: commandStateParsingCmd,
+	}
+
+	if ok := client.Scan(); !ok {
+		err := client.Err()
+		if err != nil {
+			return nil, err
+		}
+		return nil, io.EOF
+	}
+
+	text := client.Text()
+	numParams, err := strconv.Atoi(strings.TrimPrefix(text, "*"))
+	if err != nil {
+		return nil, err
+	}
+
+	for range numParams {
+		client.Scan() // Skip line containing length Ex: $4\r\n
+		client.Scan()
+		text := client.Text()
+
+		switch cmd.state {
+		case commandStateParsingCmd:
+			cmd.name = strings.ToLower(text)
+			cmd.state = commandStateParsingArgs
+		case commandStateParsingArgs:
+			cmd.args = append(cmd.args, text)
+		}
+	}
+	cmd.state = commandStateDone
+
+	return cmd, nil
+}
